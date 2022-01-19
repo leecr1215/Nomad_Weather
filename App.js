e02dfbad1cb5852bf43d460e1f43a2687365497c
leecr1215 <leecr1215@naver.com>
2022-01-19 14:58:24 +0900

    finish #2.1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!Cool</Text>
+      <Text style={styles.text}>연동되는건가!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 28,
+    color: "black",
   },
 });
